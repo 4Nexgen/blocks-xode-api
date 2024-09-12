d76fc947bc1b2ec344862eb645f1b8c09b9295ab
Arneil Paul Polican <policanarneilpaul@gmail.com>
2024-09-12 10:36:48 +0800

    adding parameter to the url

--- src/polkadot-blocks/polkadot-blocks.controller.ts
+++ src/polkadot-blocks/polkadot-blocks.controller.ts
@@ -1,5 +1,5 @@
 // polkadot-blocksScannerService.controller.ts
-import { Controller, Get, Req, Logger } from '@nestjs/common';
+import { Controller, Get, Req, Logger, Param, NotFoundException } from '@nestjs/common';
 import { ApiPromise, WsProvider  } from '@polkadot/api';
 import { environment } from 'src/dto/environment';
 import { NetworkScannerModel } from 'src/dto/network-scanner.dto';
@@ -9,19 +9,28 @@ import { BlocksScannerService } from 'src/services/polkadot.service';
 export class PolkadotBlocksController {
     private logger = new Logger('polkadot-blocks.controller');  // LOGGER INITIALIZING WITH CONTEXT
     constructor(private blocksScannerService: BlocksScannerService) {}
-    @Get()
-    async getNetworkScanner(@Req() request: Request):  Promise<any[]>{
+    
+    @Get(':network')
+    async getNetworkScanner(@Param('network') network_str: string, @Req() request: Request):  Promise<NetworkScannerModel>{
 
-        const wsProvider_local = environment.networks[0];
-        const network = wsProvider_local.network[0].wsProviderEndpoint;
-        console.log(network)
+        // const wsProvider_local = environment.networks[0];
+        const wsProvider_local = environment.networks.find((network) => network.network[0].net_name === network_str);
+        if (!wsProvider_local) {
+            throw new NotFoundException(`Network with ID ${network_str} not found`);
+        }
+        // const network = wsProvider_local.network[0].wsProviderEndpoint;
+        // console.log(network)
 
-        
+        // Example test
         // const wsProvider = new WsProvider('wss://rpc.polkadot.io');
-        const wsProvider = new WsProvider(network);
-        const api = await ApiPromise.create({ provider: wsProvider });
-        console.log(api.genesisHash.toHex());
-        this.blocksScannerService.setTotalBlocks(network)
-        return Promise.resolve([api.genesisHash.toHex()]);
+        // const wsProvider = new WsProvider(network);
+        // const api = await ApiPromise.create({ provider: wsProvider });
+        // console.log(api.genesisHash.toHex());
+
+        let network_model: NetworkScannerModel = new NetworkScannerModel();
+        network_model = await this.blocksScannerService.setTotalBlocks(wsProvider_local)
+        network_model.net_name = wsProvider_local.network[0].net_name;
+
+        return network_model;
     }
 }
--- src/services/polkadot.service.ts
+++ src/services/polkadot.service.ts
@@ -3,7 +3,7 @@ import { BehaviorSubject } from "rxjs";
 import { NetworkScannerModel } from "src/dto/network-scanner.dto";
 import { environment } from "src/dto/environment";
 import { formatBalance, BN } from '@polkadot/util';
-import { ApiPromise } from '@polkadot/api';
+import { ApiPromise, WsProvider } from '@polkadot/api';
 
 
 @Injectable()
@@ -19,67 +19,72 @@ export class BlocksScannerService{
     private tokenMetrics: BehaviorSubject<any> = new BehaviorSubject<any>(null);
     tokenMetrics$ = this.tokenMetrics.asObservable();
 
-    async scannerBlocks() {
-        console.log(`Connecting to database at ${environment.secret_key}`);
-
-
-        return this.networkScanner.asObservable();
-    }
+  async scannerBlocks() {
+      console.log(`Connecting to database at ${environment.secret_key}`);
+      return this.networkScanner.asObservable();
+  }
 
     
-  public async setTotalBlocks(params): Promise<void> {
+  public async setTotalBlocks(params): Promise<NetworkScannerModel> {
     let network: NetworkScannerModel = new NetworkScannerModel();
-    
-    console.log(`setTotalBlocks`)
-    const api = await this.api; // api initialization
-    console.log(`netwok total wallets ${api}`)
+    console.log(params)
+    let api; // api initialization
+    try {
+      await this.connect(params);
+      api = await this.api;
+    } catch (error) {
+      throw error;
+    }
 
     // Get Total Wallet
     const totalWallet: any = await api.query.system.account.entries();
     network.total_wallets = totalWallet.length
-    console.log(`netwok total wallets ${network.total_wallets}`)
-
-    // Get Net Name
-    network.net_name = params;
-    console.log(`netwok total wallets ${network.net_name}`)
 
     // Get Token Name
     const tokens = await api.registry.chainTokens;
-    console.log(`netwok total wallets ${tokens}`)
 
     // network.token_name = tokens[0]
-    // this.currentToken.next(tokens[0])
-    // this.blockSubscription = api.derive.chain.subscribeNewHeads(async (lastHeader: any) => {
-    //   const events: any = await api.query.system.events.at(lastHeader.hash);
-    //   events.forEach(async ({ event }) => {
-    //     console.log(event)
-    //       const properties = await api.rpc.system.properties();
-    //       const data: any = properties.toHuman();
-    //       const symbol = data.tokenSymbol[0];
-    //       const decimal = data.tokenDecimals[0];
-    //       formatBalance.setDefaults({ decimals: decimal, unit: symbol });
-    //       formatBalance.getDefaults();
-    //       const bal = formatBalance(
-    //         event.data.actualFee,
-    //         {
-    //           forceUnit: symbol,
-    //           withUnit: false
-    //         }
-    //       );
-    //       const balances = parseFloat(bal).toFixed(6);
-    //       const fee_format = `${balances}`;
-    //       network.last_gas_fee = parseFloat(fee_format) == 0.000000 ? 0.00212 : parseFloat(fee_format);
-    //   });
-  
-    //   network.total_blocks = lastHeader.number.toNumber();
-    //   this.networkScanner.next(network)
-    // });
+    let last_gas_fee =0; 
+    let total_blocks = '';
+    this.currentToken.next(tokens[0])
+    await new Promise<void>((resolve) => {
+      this.blockSubscription = api.derive.chain.subscribeNewHeads(async (lastHeader: any) => {
+        const events: any = await api.query.system.events.at(lastHeader.hash);
+        events.forEach(async ({ event }) => {
+          // console.log(event)
+            const properties = await api.rpc.system.properties();
+            const data: any = properties.toHuman();
+            const symbol = data.tokenSymbol[0];
+            const decimal = data.tokenDecimals[0];
+            formatBalance.setDefaults({ decimals: decimal, unit: symbol });
+            formatBalance.getDefaults();
+            const bal = formatBalance(
+              event.data.actualFee,
+              {
+                forceUnit: symbol,
+                withUnit: false
+              }
+            );
+            const balances = parseFloat(bal).toFixed(6);
+            const fee_format = `${balances}`;
+            
+            network.last_gas_fee = parseFloat(fee_format) == 0.000000 ? 0.00212 : parseFloat(fee_format);
+            last_gas_fee = network.last_gas_fee
+            
+          });
+          total_blocks = (lastHeader.number).toString();
+          console.log(`Last gas fee ${last_gas_fee}`)
+          network.last_gas_fee = last_gas_fee;
+          network.total_blocks = total_blocks;
+          resolve();
+      });
+    });
+
+    return network
   }
 
-  async connect(){
-    const provider = environment.networks[1].network[0].wsProviderEndpoint
-    // this.contractAddress = this.cookiesService.getCookie('contract_address');
+  async connect(params){
+    const provider = new WsProvider(params.network[0].wsProviderEndpoint)
     this.api = ApiPromise.create({ provider });
   }
-
 }
